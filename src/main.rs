47255e0890d3820acc9fb87591bb9e16a4134124
RichoKD <jerrykaduma@gmail.com>
2025-06-04 15:30:09 +0100

    setup

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+fn get_cpu_thread_count() -> usize {
+    // This function would typically use a system call or library to get the CPU thread count.
+    // For demonstration purposes, we'll return a hardcoded value.
+    6
+}
